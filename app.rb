0d614e293cda4c637ed80273281ff0fe519eea2c
Jonathan Norman <jnorman2123@gmail.com>
2019-11-13 18:34:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Jonathan."
+  end
+
+  get '/hometown' do
+    "My hometown is Atlanta, Ga."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Passenger Seat by Death Cab for Cutie."
+  end
 end
