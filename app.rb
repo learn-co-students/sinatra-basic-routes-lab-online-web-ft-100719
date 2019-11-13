9f18466d35cae7a3fdccdfe458962a8998117fe1
Kathleen Kamali <kathleenkamali@gmail.com>
2019-11-13 11:11:21 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get "/name" do
+    "My name is Kathleen"
+  end
+
+  get "/hometown" do
+    "My hometown is Seattle"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is 'Come Along' by Cosmo Shelton"
+  end
 end
