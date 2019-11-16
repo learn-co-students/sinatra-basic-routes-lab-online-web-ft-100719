b0eaafb8fccd2361584e9619565d179c06fbfd20
Huda Yousif <hjoski@gmail.com>
2019-11-15 23:10:54 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+    "My name is Huda."
+    end
+
+    get '/hometown' do
+    "My hometown is San Diego. "
+    end
+
+    get '/favorite-song' do
+    "My favorite song is Over and Over again"
+    end
+
 end
