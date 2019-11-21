0dbc673c9234ec8a3ee96b2f4d2e692f5b7978a4
lmjcbs <hello@liamjacobs.dev>
2019-11-21 14:21:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Liam"
+  end
+
+  get '/hometown' do
+    "My hometown is London"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Glue"
+  end
+
 end
