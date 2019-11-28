401f74db3fdecb09de28d20f777f1132de9016dc
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-11-28 20:24:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Rokhaya"
+  end
+
+  get '/hometown'do
+    "My hometown is Dakar "
+  end
+  
+  get '/favorite song'do
+    "My favorite song is joro wizkid" 
+  end
 end
